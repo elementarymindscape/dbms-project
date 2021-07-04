703605e628f133e5588c009dcd6a45aae54cd741
elementarymindscape <sharma.vv13@gmail.com>
2021-07-04 18:31:09 +0530

    working frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Part</h1>
-    </div>
-  );
-}
+    const  App = () =>{
+
+      const [userName,setUserName]= useState('');
+      const [password,setPassword]= useState('');
+
+      const submitUser = async() => {
+        axios.post("http://localhost:3001/api/registeruser", {
+          username: userName,
+          password: password,
+      }).then((Response)=> 
+      {console.log(Response)
+      });
+      };
+
+     return(
+      <div className='form'>
+        <h1>HEADING</h1>
+        <input placeholder='Username' type='text' name="userName" onChange={(e) => setUserName(e.target.value)} ></input>
+        <input placeholder='Password' type='password' name="password" onChange={(e) => setPassword(e.target.value)} ></input>
+        <button type='submit'  onClick={submitUser}>SUBMIT</button>
+      </div>
+   );
+        
+   }
+
+
+ 
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!")
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Running on port 3000")
-}) 
+const db = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "Vivekk",
+    password: "1234QwertY_0502",
+    database: "project_database"
+});
+
+db.connect();
+
+app.post("/api/registeruser", (req, res)=>{
+    const sqlInsertUser = ' INSERT into userstable (userName, password) VALUES (?,?); '
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(sqlInsertUser, [username, password], (error, results)=>{
+        if (error){
+            console.log(errors);
+        }
+        console.log(results)
+    })
+});
+
+app.get("/api/registeruser", (req, res) => {
+    res.send("Welcome");
+});
+
+app.listen(3001, ()=>{
+    console.log("listening to port 3001")
+})
